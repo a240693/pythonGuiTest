d654f44c02c01e03ed27491107148f90e671b3cd
Ylx <461605470@qq.com>
2022-06-29 02:13:28 +0800

    2022年6月29日02:12:18 1、工具类完成，现在传入图片队列可以自动搜索图片内的数组，完成后可自动返回点击直到找到结束图片。

--- dao/airMultiPhotos.py
+++ dao/airMultiPhotos.py
@@ -1,86 +1,53 @@
 import pyautogui
 import time
 # from . import test
-from . import dao
 from . import changeVar as cv
+from airtest.core.api import *
+from airtest.report.report import simple_report
+from poco.drivers.android.uiautomation import AndroidUiautomationPoco
+
+poco = AndroidUiautomationPoco(use_airtest_input=True, screenshot_each_action=False)
+
+auto_setup(__file__, devices=["Android://127.0.0.1:5037/emulator-5560"])
 
 path = cv.FgoPath
+
+
 # path = 'D:\\pyTest\\'
 
 class Photo:
     def __init__(self):
         self.name = "默认"
-        self.x = 0
-        self.y = 0
+        self.pos = (0, 0)
 
-    def writeSelf(self, name, x, y):
+    def writeSelf(self, name, pos):
         self.name = name
-        self.x = x
-        self.y = y
+        self.pos = pos
 
-    def onlySearchOnce(self, name, mode, times):
-        try:
-            filepath = path + name + '.png'
-            img = dao.my_cv_imread(filepath)  # 获取读取的图像
-            x, y, w, h = pyautogui.locateOnScreen(img, grayscale=False, confidence=0.9)
-            x, y = pyautogui.center((x, y, w, h))
-            self.writeSelf(name, x, y)
-            print("{}.png在屏幕中的位置是：X={},Y={}，宽{}像素,高{}像素".format(name, x, y, w, h))
-            return 1
-        except Exception as e:
-            # print(e)
-            return 0
-
-    # 随便你传几张照片进来，找到哪张就返回哪张照片的坐标和名字。
-    def loopSearch(self, gamePagesMap):
-        count = 0
-        while True:
-            for i in gamePagesMap:
-                # print("开始查找{}".format(i))
-                if 0 != self.onlySearchOnce(i, 3, 3):
-                    return self
-            count += 1
-            if count == 5:
-                print("找不到5次，休息0.3秒")
-                time.sleep(0.3)
-                count = 0
-
-    # 进入游戏用，找不到会自动点击左键一次。
-    def firstClickSearch(self, gamePagesMap):
-        count = 0
-        while True:
-            for i in gamePagesMap:
-                if 0 != self.onlySearchOnce(i, 3, 3):
-                    return self
-                else:
-                    pyautogui.click(button="left")
-                    time.sleep(5)
-            count += 1
-            if count == 5:
-                print("找不到5次，休息0.3秒")
-                time.sleep(0.3)
-                count = 0
-
-    def searchPhoto(self, name):
-        count = 0
-        found = False
-        while not found:
-            try:
-                filepath = path + name + '.png'
-                img = dao.my_cv_imread(filepath)  # 获取读取的图像
-                x, y, w, h = pyautogui.locateOnScreen(img, grayscale=True, confidence=0.9)
-                x, y = pyautogui.center((x, y, w, h))
-                print("{}.png在屏幕中的位置是：X={},Y={}，宽{}像素,高{}像素".format(name, x, y, w, h))
-                found = True
-                self.writeSelf(name, x, y)
+    # 循环 找到图片就返回坐标和名字回最顶层。
+    def loopSearch(self, photoMaps):
+        for photoMap in photoMaps:
+            result = self.appearThenClick(photoMap)
+            if 1 == result:
                 return self
-            except:
-                count = count + 1
-                print('{}.png没找到，第{}次,3秒后重试'.format(name, count))
-                time.sleep(3)
-                if count > 20:
-                    print("超过{}次没找到，判断为卡住，请人工协助。".format(count))
-                    exit()
+            elif 0 == result:
+                continue
+
+    # 查找图片，3秒内判断该图片有没有出现，没有则找下一张。
+    # 有就返回1，无就返回0.
+    def appearThenClick(self, photoMap):
+        try:
+            photo = Template(path + photoMap + ".png")
+            pos = exists(photo)
+            if pos:
+                self.writeSelf(photoMap, pos)
+                return 1
+            else:
+                return 0
+        except:
+            return 0
+        finally:
+            simple_report(__file__, logpath=True)
 
     def __str__(self):
-        return print(self.name, self.x, self.y)
+        return print(self.name, self.pos)
--- games/fgo.py
+++ games/fgo.py
@@ -2,22 +2,18 @@
 from airtest.core.api import *
 from airtest.report.report import simple_report
 from poco.drivers.android.uiautomation import AndroidUiautomationPoco
+from dao import airMultiPhotos as air
 
 poco = AndroidUiautomationPoco(use_airtest_input=True, screenshot_each_action=False)
 
-auto_setup(__file__,devices=["Android://127.0.0.1:5037/emulator-5554"])
+auto_setup(__file__,devices=["Android://127.0.0.1:5037/emulator-5560"])
 
 path = 'F:\\pyTest\\'
 
 def enterGame():
+    photoMap = air.Photo()
     photoMaps = [
-        # "fgo",
-        "迦勒底之门",
-        "每日任务",
-        "种火40",
-        "狂阶",
-        "宝石翁",
-        "开始任务",
+        "fgo",
     ]
     moveMaps = [
         (943, 136),  # 滑动起点。
@@ -25,16 +21,8 @@ def enterGame():
         (943, 177),  # 种火滑动。
     ]
     try:
-        # print(poco.adb_client.get_device_info())  # 获取设备信息
-        for i in photoMaps:
-            # 获取图片，不操作。
-            temp = Template(path + i + ".png")
-            # print(temp)
-            if (not exists(temp)) & ("40" not in i):
-                swipe(moveMaps[0], moveMaps[1])
-            elif (not exists(temp)) & ("40" in i):
-                swipe(moveMaps[0], moveMaps[2])
-            touch(temp)
+        photoMap.loopSearch(photoMaps)
+        print(photoMap.name)
     finally:
         # generate html report
         simple_report(__file__, logpath=True)
@@ -69,5 +57,5 @@ def battle():
         simple_report(__file__, logpath=True)
 
 if __name__ == "__main__":
-    battle()
+    enterGame()
 
