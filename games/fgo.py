792ec6194c8117ce9ba31ef2a1ef6632a1df64b0
yao.leixin <0027011403@iwhalecloud.com>
2022-06-28 22:08:55 +0800

    2022/6/28， 22点08分 1、基于网易airTest新增FGO模块。

--- games/fgo.py
+++ games/fgo.py
@@ -5,16 +5,69 @@ from poco.drivers.android.uiautomation import AndroidUiautomationPoco
 
 poco = AndroidUiautomationPoco(use_airtest_input=True, screenshot_each_action=False)
 
-auto_setup(__file__,devices=["Android://127.0.0.1:7912/emulator-5560"])
+auto_setup(__file__,devices=["Android://127.0.0.1:5037/emulator-5554"])
 
-path = 'F:\\pyTest\\fgo\\'
+path = 'F:\\pyTest\\'
 
-if __name__ == "__main__":
+def enterGame():
+    photoMaps = [
+        # "fgo",
+        "迦勒底之门",
+        "每日任务",
+        "种火40",
+        "狂阶",
+        "宝石翁",
+        "开始任务",
+    ]
+    moveMaps = [
+        (943, 136),  # 滑动起点。
+        (943, 282),  # 每日滑动终点
+        (943, 177),  # 种火滑动。
+    ]
     try:
-        print(poco.adb_client.get_device_info())  # 获取设备信息
-        temp = Template(path +"fgo.png", record_pos=(0.779, 0.382), resolution=(720, 1280))
-        print(temp)
-        touch(temp)
+        # print(poco.adb_client.get_device_info())  # 获取设备信息
+        for i in photoMaps:
+            # 获取图片，不操作。
+            temp = Template(path + i + ".png")
+            # print(temp)
+            if (not exists(temp)) & ("40" not in i):
+                swipe(moveMaps[0], moveMaps[1])
+            elif (not exists(temp)) & ("40" in i):
+                swipe(moveMaps[0], moveMaps[2])
+            touch(temp)
     finally:
         # generate html report
         simple_report(__file__, logpath=True)
+
+def battle():
+    photoMaps = [
+        # "技能2",
+        # "攻击",
+        "战斗界面",
+    ]
+    moveMaps = [
+        (310,152), # 一宝具
+        (486,152), # 二宝具
+        (657,152), # 三宝具
+        (98,376), # 卡1
+        (289,376), # 卡2
+    ]
+    try:
+        # print(poco.adb_client.get_device_info())  # 获取设备信息
+        for i in photoMaps:
+            # 获取图片，不操作。
+            temp = Template(path + i + ".png")
+            # print(temp)
+            if "界面" in i:
+                for step in moveMaps:
+                    touch(step)
+            if not exists(temp):
+                continue
+            # touch(temp)
+    finally:
+        # generate html report
+        simple_report(__file__, logpath=True)
+
+if __name__ == "__main__":
+    battle()
+
--- testTools/adbTest.py
+++ testTools/adbTest.py
@@ -1,18 +1,18 @@
-#emulator-5566
+# emulator-5566
 from airtest.core.api import *
 from airtest.report.report import simple_report
 from poco.drivers.android.uiautomation import AndroidUiautomationPoco
 
 poco = AndroidUiautomationPoco(use_airtest_input=True, screenshot_each_action=False)
 
-auto_setup(__file__,devices=["Android://127.0.0.1:5037/emulator-5566"])
+auto_setup(__file__, devices=["Android://127.0.0.1:5037/emulator-5554"])
 
 path = 'F:\\pyTest\\'
 
 if __name__ == "__main__":
     try:
         print(poco.adb_client.get_device_info())  # 获取设备信息
-        temp = Template(path +"mrfz.png", record_pos=(0.779, 0.382), resolution=(720, 1280))
+        temp = Template(path +"fgo.png", record_pos=(0.779, 0.382), resolution=(720, 1280))
         print(temp)
         touch(temp)
     finally:
