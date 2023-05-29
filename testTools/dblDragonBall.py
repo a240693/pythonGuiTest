66e649156aca5b957183f4c688fb2f0f5e164355
ylx <461605470@qq.com>
2023-05-29 21:08:49 +0800

    2023年5月29日21:08:37 1、自动生成二维码试做。

---
+++ testTools/dblDragonBall.py
@@ -0,0 +1,12 @@
+
+def autoGet(number):
+        print("4,5yb8upz3+CKK{}".format(number)+"AAAA")
+        print("4,n7z5y3az+CKK{}".format(number)+"AAAA")
+        print("4,u34gb7a4+CKK{}".format(number)+"AAAA")
+
+
+
+
+if __name__ == "__main__":
+    number = "HJGH"
+    autoGet(number)
