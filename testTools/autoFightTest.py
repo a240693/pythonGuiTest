28654ace4a8ccf4bb8b17b6d6491a2c50579f9b2
Ylx <461605470@qq.com>
2024-10-02 16:06:01 +0800

    2024年10月2日16:05:33 1、效率优化，换包，pynput屁用么得，直接用原生keyboard

--- testTools/autoFightTest.py
+++ testTools/autoFightTest.py
@@ -1,11 +1,22 @@
 import pyautogui
 import time
 import _thread
-from pynput import keyboard
+import keyboard
+# from pynput import keyboard
 
 global flag
 flag = False
 
+global keys
+keys = []
+
+global oldKey
+oldKey = "0"
+
+global h
+
+global testFlag
+testFlag = 0
 
 def attackTest():
     print("开始自动重击".format())
@@ -17,6 +28,7 @@ def attackTest():
             pyautogui.mouseDown(button="left")
             time.sleep(0.5)
             pyautogui.mouseUp(button="left")
+        time.sleep(2)
 
 
 def changeTeam():
@@ -34,13 +46,12 @@ def changeTeam():
                 time.sleep(2)
                 if not flag:
                     break
-        # dao.pressKey('r')11
+        # dao.pressKey('r')
+        time.sleep(2)
 
 
 def startAutoFight():
     try:
-        print("给你两秒切过去。".format())
-        time.sleep(2)
         print("自动战斗测试".format())
         _thread.start_new_thread(attackTest, ())
         _thread.start_new_thread(changeTeam, ())
@@ -52,18 +63,29 @@ def startAutoFight():
 
 
 def on_press(event):
-    # tmd， 没办法了，event不会自动刷新所以一直按，真铸币，2024年10月1日16:24:43
-    print("输入：", event)
+    global keys
+    global oldKey
+    # print(event)
+    if str(event) != oldKey:
+        oldKey = str(event)
+        keys.append(str(event))
+
+def duelKeys():
     global flag
-    if "Key.f9".__eq__(str(event)):
-        # print("结果是：","Key.f3".__eq__(str(event)))
-        flag = True
-        print("开启自动战斗")
-
-    if "Key.f10".__eq__(str(event)):
-        flag = False
-        print("关闭自动战斗")
-
+    while 1:
+        if keys.__len__() != 0:
+            key = keys.pop()
+            print("当前值为：{}".format(key))
+            # if "Key.f9".__eq__(key):
+            #     # print("结果是：","Key.f3".__eq__(str(event)))
+            #     flag = True
+            #     print("开启自动战斗")
+            #     continue
+            #
+            # if "Key.f10".__eq__(key):
+            #     flag = False
+            #     print("关闭自动战斗")
+            #     continue
 
 def on_release(event):
     # print("松开的是：", event)
@@ -71,9 +93,76 @@ def on_release(event):
 
 
 def pynputTest():
-    listener = keyboard.Listener(
-        on_press=on_press, on_release=on_release)
-    listener.start()
+    # listener = keyboard.Listener(
+    #     on_press=on_press,
+    #     # on_release=on_release
+    # )
+    # listener.start()
+    # 第二种
+    # with keyboard.Listener(on_press=on_press) as listener:
+    #     listener.join()
+    # 第三种 热键获取
+    # with keyboard.GlobalHotKeys({
+    #     '<f9>': f9test,
+    #     '<f10>': f10test,
+    # }) as h:
+    #     h.join()
+    # 第四种 热键获取
+    global h
+    print("线程开启。")
+    h = keyboard.GlobalHotKeys({
+        # 'a': pressSpace,
+        # 's': pressSpace,
+        # 'd': pressSpace,
+        # 'w': pressSpace,
+        '<f9>': f9test,
+        '<f10>': f10test,
+    })
+    h.start()
+
+def f9test():
+    global flag
+    flag = True
+    print("开启自动战斗。")
+    # h.stop()
+    # time.sleep(10)
+    global testFlag
+    testFlag = 1
+
+def f10test():
+    global flag
+    flag = False
+    # print("关闭自动战斗,线程休眠3秒。")
+    print("关闭自动战斗。")
+    # h.stop()ree
+    # time.sleep(10)
+    # 已选择，线程休眠3秒。
+    global testFlag
+    testFlag = 1
+
+def pressSpace():
+    # h.stop()
+    print("检测到方向键,线程休眠3秒。")
+    time.sleep(10)
+    global testFlag
+    testFlag = 1
+
+def keyboardTest():
+    while 1:
+        if keyboard.is_pressed("f9"):
+            # print("检测到F9。")
+            f9test()
+            break
+
+        if keyboard.is_pressed("f10"):
+            # print("检测到F10。")
+            f10test()
+            break
+
+        time.sleep(1)
+
+
+
 
 
 if __name__ == '__main__':
@@ -81,7 +170,14 @@ if __name__ == '__main__':
     # time.sleep(1)
     # pyautogui.mouseUp(button="left")
     # dao.pressKey('pagedown')
-    pynputTest()
+    # pynputTest()
     startAutoFight()
+    while 1:
+        keyboardTest()
+    # _thread.start_new_thread(startAutoFight,())
+    # _thread.start_new_thread(duelKeys,())
     while True:
+        # if testFlag == 1:
+        #     pynputTest()
+        #     testFlag = 0
         pass
